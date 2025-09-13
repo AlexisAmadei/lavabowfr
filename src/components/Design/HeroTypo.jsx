8cd27e4340e67cc5b3c479dda3de4fa085f25464
Alexis Amadei <alexisamadei@gmail.com>
2025-09-14 01:14:02 +0200

    try a compensation for lineheight of hero typo

--- src/components/Design/HeroTypo.jsx
+++ src/components/Design/HeroTypo.jsx
@@ -4,8 +4,8 @@ export default function HeroTypo() {
     return (
         <div className="hero-typo">
             <p>LAVA BOW</p>
-            <p id='outline'>LAVA BOW</p>
+            <p id="outline">LAVA BOW</p>
             <p>LAVA BOW</p>
         </div>
-    )
+    );
 }
