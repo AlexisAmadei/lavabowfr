6c4275cdd3d6c36913ce9608685801a01fb9fec7
alexis <alexisamadei@gmail.com>
2025-09-15 21:33:00 +0200

    prettier

--- src/components/Design/Section.jsx
+++ src/components/Design/Section.jsx
@@ -4,7 +4,14 @@ import LavaTypo from './LavaTypo'
 
 export default function Section({ children, bgImage, bgColor, title }) {
     return (
-        <div className='lava-section' style={{ backgroundImage: `url(${bgImage})`, backgroundColor: bgColor, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className='lava-section'
+            style={{
+                backgroundImage: `url(${bgImage})`,
+                backgroundColor: bgColor,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center'
+            }}
+        >
             {title && <LavaTypo variant={'h2'}>{title}</LavaTypo>}
             {children}
         </div>
