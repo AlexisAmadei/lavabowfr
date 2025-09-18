a7b708d80ae53ee187cd00bca181cafc16f54696
alexis <alexisamadei@gmail.com>
2025-09-19 01:24:55 +0200

    add compact disk to music section

---
+++ src/components/Core/CompactDisc/CompactDisc.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import './CompactDisc.css';
+
+export default function CompactDisc() {
+    return (
+        <Box className='cd-container' width={'100%'} minWidth={'250px'} maxWidth={'350px'}>
+            <Box className='cd'>
+                <Box className='cd-inner'>
+                    <Box className='cd-label'>
+                        <Box className='cd-hole'></Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
--- src/components/Sections/Music.jsx
+++ src/components/Sections/Music.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import LavaTypo from '@/components/Design/LavaTypo'
 import Section from '@/components/Design/Section'
 import './styles/Music.css'
+import { Flex } from '@chakra-ui/react'
+import CompactDisc from '../Core/CompactDisc/CompactDisc'
 
 export default function Music() {
     return (
@@ -10,14 +12,23 @@ export default function Music() {
             id='music'
         >
             <LavaTypo variant='h1'>Notre Musique</LavaTypo>
-            <iframe data-testid="embed-iframe"
-            style={{ borderRadius: '12px' }}
-            src="https://open.spotify.com/embed/artist/54Y8JDIfmIb2zlHrB2ZoVF?utm_source=generator&theme=0"
-            width="100%"
-            height="352"
-            loading="lazy"
-            name='spotify-embed'
-            ></iframe>
+            <Flex
+                width={'100%' }
+                direction={'row'}
+                alignItems={'center'}
+                justifyContent={'space-around'}
+                gap={10}
+            >
+                <CompactDisc />
+                <iframe data-testid="embed-iframe"
+                    style={{ borderRadius: '12px' }}
+                    src="https://open.spotify.com/embed/artist/54Y8JDIfmIb2zlHrB2ZoVF?utm_source=generator&theme=0"
+                    width="100%"
+                    height="352"
+                    loading="lazy"
+                    name='spotify-embed'
+                ></iframe>
+            </Flex>
         </Section>
     )
 }
