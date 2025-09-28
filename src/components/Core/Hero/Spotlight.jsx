9916965752d39c3967b67e63acb496e34ad38dcd
alexis <alexisamadei@gmail.com>
2025-09-28 20:01:31 +0200

    css: adjust style for spotlight component

--- src/components/Core/Hero/Spotlight.jsx
+++ src/components/Core/Hero/Spotlight.jsx
@@ -6,6 +6,7 @@ import useIsMobile from '../../../hooks/useIsMobile'
 
 export default function Spotlight() {
   const isMobile = useIsMobile();
+  const mP = isMobile ? '12px 24px' : '12px 32px';
 
   return (
     <Flex direction={'column'} alignItems={isMobile ? 'center' : 'flex-end'} width={isMobile ? '100%' : 'auto'}>
@@ -14,11 +15,11 @@ export default function Spotlight() {
       <LavaTypo variant={'text'} styles={{ marginBottom: !isMobile ? '24px' : '' }}>Notre dernier single disponible partout</LavaTypo>
 
       <Flex direction={'row'} gap={4} marginTop={4}>
-        <LavaButton variant='filled' padding={'12px 40px'}>
-          <LavaTypo variant='text' size={'24px'}>Écouter</LavaTypo>
+        <LavaButton variant='filled' padding={mP}>
+          <LavaTypo variant='text' size={isMobile ? '6vw' : '24px'}>Écouter</LavaTypo>
         </LavaButton>
-        <LavaButton variant='outlined' padding={'12px 40px'}>
-          <LavaTypo variant='text' size={'24px'}>Acheter</LavaTypo>
+        <LavaButton variant='outlined' padding={mP}>
+          <LavaTypo variant='text' size={isMobile ? '6vw' : '24px'}>Acheter</LavaTypo>
         </LavaButton>
       </Flex>
 
