7b992c34c402ee7482502eacb9d1bae4e60066f1
alexis <alexisamadei@gmail.com>
2025-11-16 23:11:45 +0100

    feat: rework socials list

--- src/lib/socialLinks.js
+++ src/lib/socialLinks.js
@@ -1,9 +1,19 @@
-import { FaSpotify } from "react-icons/fa";
-import { LuFacebook, LuInstagram, LuYoutube } from "react-icons/lu";
+import {
+    FaApple,
+    FaBandcamp,
+    FaDeezer,
+    FaInstagram,
+    FaSpotify,
+    FaSquareFacebook,
+    FaYoutube
+} from "react-icons/fa6";
 
 export const socialLinks = [
-    { instagram: 'https://www.instagram.com/lava_bow/', icon: LuInstagram },
-    { facebook: 'https://www.facebook.com/lavabow', icon: LuFacebook },
-    { spotify: 'https://open.spotify.com/artist/54Y8JDIfmIb2zlHrB2ZoVF', icon: FaSpotify },
-    { youtube: 'https://www.youtube.com/@lavabow', icon: LuYoutube },
+    { name: 'instagram', url: 'https://www.instagram.com/lava_bow/', icon: FaInstagram },
+    { name: 'facebook', url: 'https://www.facebook.com/lavabow', icon: FaSquareFacebook },
+    { name: 'youtube', url: 'https://www.youtube.com/@lavabow', icon: FaYoutube },
+    { name: 'bandcamp', url: '#', icon: FaBandcamp },
+    { name: 'apple', url: '#', icon: FaApple },
+    { name: 'deezer', url: '#', icon: FaDeezer },
+    { name: 'spotify', url: 'https://open.spotify.com/artist/54Y8JDIfmIb2zlHrB2ZoVF', icon: FaSpotify },
 ];
