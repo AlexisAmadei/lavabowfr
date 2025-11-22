5f955c40c5a934769172415cca2fa7d56986bad0
alexis <alexisamadei@gmail.com>
2025-11-22 12:48:00 +0100

    move admin menu items

--- src/views/Admin/Dashboard.jsx
+++ src/views/Admin/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Link, useLocation, Outlet } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import AdminMenuItems from '@/constants/AdminMenuItems';
+import AdminMenuItems from '@/components/Core/Admin/AdminMenuItems';
 
 export default function Dashboard() {
   const location = useLocation();
