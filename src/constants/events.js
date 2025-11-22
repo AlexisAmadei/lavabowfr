39932b6eaa7f1aa0e65d3aeb901272626fb45746
alexis <alexisamadei@gmail.com>
2025-11-22 12:47:14 +0100

    delete old unused const files

--- src/constants/events.js
+++
@@ -1,8 +0,0 @@
-/**
- * Default values for event data
- */
-export const EVENT_DEFAULTS = {
-  title: "Titre de l'event",
-  date: "20/03/2025",
-  description: "Découvrez les moments forts de cet événement exceptionnel. Une expérience unique remplie d'émotions et de souvenirs inoubliables."
-}
