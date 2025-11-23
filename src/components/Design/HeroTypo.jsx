738b1adede31f2274638ced23df9873f937ca460
alexis <alexisamadei@gmail.com>
2025-11-23 17:48:35 +0100

    fix: take window height in consideration for LAVABOW hero typo

--- src/components/Design/HeroTypo.jsx
+++ src/components/Design/HeroTypo.jsx
@@ -8,6 +8,7 @@ import HeroOutlined from '@/assets/img/HeroTypo/hero-outlined.svg';
 
 export default function HeroTypo({ fontSize = 15, repeated = false }) {
   const isMobile = useIsMobile();
+  const smallHeight = window.innerHeight < 800;
 
   return (
     <Box className='hero-typo' width={'100%'}>
@@ -18,7 +19,7 @@ export default function HeroTypo({ fontSize = 15, repeated = false }) {
         (
           <img src={HeroOutlined} width={'100%'} height={'auto'} alt='Lava Bow Hero Typo Outlined' />
         )}
-      {!repeated && (
+      {!repeated && !smallHeight && (
         <img src={HeroFilled} width={'100%'} height={'auto'} alt='Lava Bow Hero Typo Filled' />
       )}
     </Box>
