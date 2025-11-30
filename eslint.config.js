dce1dbedaf8700f01c89b1431b5be4ccb510a866
alexis <alexisamadei@gmail.com>
2025-11-30 13:29:04 +0100

    fix: update ESLint configuration to support TypeScript and add TypeScript ESLint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,13 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-    globalIgnores(['dist']),
+    globalIgnores(['dist', 'api']),
     {
-        files: ['**/*.{js,jsx,ts,tsx}'],
+        files: ['**/*.{js,jsx}'],
         extends: [
             js.configs.recommended,
             reactHooks.configs['recommended-latest'],
@@ -24,6 +25,30 @@ export default defineConfig([
         },
         rules: {
             'no-unused-vars': 'warn',
+        },
+    },
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+                project: './tsconfig.json',
+            },
+        },
+        rules: {
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
         },
     },
 ])
