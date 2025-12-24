32f1b7a7c39f1fff8d1d8a54b0898fef9c5eb384
alexis <alexisamadei@gmail.com>
2025-12-24 01:11:12 +0100

    feat: refacto menuItems for new appbar

--- src/lib/menuItems.tsx
+++ src/lib/menuItems.tsx
@@ -1,13 +1,38 @@
 import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const menuItems = [
-  { name: 'À propos', link: '#about', variant: 'outlined' },
-  { name: 'Concerts', link: '#events', variant: 'outlined' },
-  { name: 'Musique', link: '#music', variant: 'outlined' },
-  { name: 'Vidéos', link: '#videos', variant: 'outlined' },
-  { name: 'Photos', link: '#photos', variant: 'outlined' },
-  { name: 'Contact', link: '#contact', variant: 'filled', icon: () => <FontAwesomeIcon icon={faArrowRightLong} /> },
+export interface MenuItem {
+  name: string;
+  link: string;
+  variant: 'filled' | 'outlined';
+  subItems?: MenuItem[];
+  icon?: React.ComponentType<{ strokeWidth?: number; size?: number }>;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'À propos',
+    link: '#about',
+    variant: 'outlined',
+    subItems: [
+      { name: 'À propos', link: '#about', variant: 'outlined' },
+      { name: 'Musique', link: '#music', variant: 'outlined' },
+      { name: 'Évènements', link: '#events', variant: 'outlined' },
+      { name: 'Vidéos', link: '#videos', variant: 'outlined' },
+      { name: 'Photos', link: '#photos', variant: 'outlined' },
+    ]
+  },
+  {
+    name: 'Merch',
+    link: '/shop',
+    variant: 'outlined',
+  },
+  {
+    name: 'Contact',
+    link: '#contact',
+    variant: 'filled',
+    icon: () => <FontAwesomeIcon icon={faArrowRightLong} />
+  },
 ]
 
 export default menuItems
