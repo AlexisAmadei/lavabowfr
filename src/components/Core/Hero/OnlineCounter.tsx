67c2b2ebbe44ed5a9d9363e6d1022c5e8350a560
alexis <alexisamadei@gmail.com>
2026-01-02 17:48:24 +0100

    temp: mute online counter while fixing

--- src/components/Core/Hero/OnlineCounter.tsx
+++ src/components/Core/Hero/OnlineCounter.tsx
@@ -8,23 +8,24 @@ import './OnlineCounter.css'
 export default function OnlineCounter() {
   const [onlineUsers, setOnlineUsers] = useState(0);
 
-  useEffect(() => {
-    const eventSource = new EventSource('/api/online-polling');
+  // useEffect(() => {
+  //   const eventSource = new EventSource('/api/online-polling');
 
-    eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      setOnlineUsers(data.onlineUsers);
-    };
+  //   eventSource.onmessage = (event) => {
+  //     const data = JSON.parse(event.data);
+  //     setOnlineUsers(data.onlineUsers);
+  //   };
 
-    return () => {
-      eventSource.close();
-    };
-  }, []);
+  //   return () => {
+  //     eventSource.close();
+  //   };
+  // }, []);
 
   return (
-    <Box display={'inline-flex'} alignItems={'center'} gap={2}>
-      <LavaTypo variant='p' className='online-counter'>{onlineUsers}</LavaTypo>
-      <FontAwesomeIcon icon={faUserGroup} size='lg' />
-    </Box>
+    // <Box display={'inline-flex'} alignItems={'center'} gap={2}>
+    //   <LavaTypo variant='p' className='online-counter'>{onlineUsers}</LavaTypo>
+    //   <FontAwesomeIcon icon={faUserGroup} size='lg' />
+    // </Box>
+    <></>
   )
 }
