637d5444158b6e5e1262b377cfe0c00c229311bc
alexis <alexisamadei@gmail.com>
2026-03-21 18:52:07 +0100

    db: update seed for local dev

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,12 +1,12 @@
 insert into section_spotlight (title, subtitle, listen_link, status)
 values
-    ('Spotlight Item 1', 'Subtitle for item 1', 'https://example.com/listen1', 'active'),
-    ('Spotlight Item 2', 'Subtitle for item 2', 'https://example.com/listen2', 'inactive');
+    ('Spotlight Item 1', 'Subtitle for item 1', 'https://example.com/listen1', 'ACTIVE'),
+    ('Spotlight Item 2', 'Subtitle for item 2', 'https://example.com/listen2', 'INACTIVE');
 
-insert into section_videos (description, url, status)
+insert into section_videos (description, url, status, "order")
 values
-    ('Video 1 Description', 'https://example.com/video1', 'active'),
-    ('Video 2 Description', 'https://example.com/video2', 'inactive');
+    ('Video 1 Description', 'https://example.com/video1', 'ACTIVE', 1),
+    ('Video 2 Description', 'https://example.com/video2', 'INACTIVE', 2);
 
 insert into clicks_paliers (name, target)
 values
