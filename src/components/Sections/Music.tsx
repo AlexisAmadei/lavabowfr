9cda5c57ea26706849e1f6d84880be5013db7615
alexis <alexisamadei@gmail.com>
2026-03-22 22:15:43 +0100

    fix: update music section to include new singles and improve iframe structure

--- src/components/Sections/Music.tsx
+++ src/components/Sections/Music.tsx
@@ -31,16 +31,20 @@ export default function Music() {
           width={isMobile ? '100%' : '60%'}
           marginTop={isMobile ? 8 : 0}
         >
-          <LavaTypo variant={'h2'}>Dernière sortie</LavaTypo>
+          <LavaTypo variant={'h2'}>LOVE, CHEER & BEERS, les singles</LavaTypo>
 
-          {/* Apple embed */}
-          <iframe
-            allow="autoplay ; encrypted-media; fullscreen *; clipboard-write"
-            height="175"
-            style={{ width: '100%', overflow: 'hidden', borderRadius: '4px' }}
-            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-            src="https://embed.music.apple.com/fr/song/smoking-man-in-a-cave-feat-lea/1845660528"
-          ></iframe>
+          <Flex
+            direction={'column'}
+            alignItems={'center'}
+            justifyContent={'flex-start'}
+            width={'100%'}
+            gap={2}
+          >
+            <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" height="175" width={'100%'} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/grief-song/1876511110?i=1876511111&theme=auto"></iframe>
+            <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" height="175" width={'100%'} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/smoking-man-in-a-cave-feat-lea/1845660527?i=1845660528&theme=auto"></iframe>
+            <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" height="175" width={'100%'} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/big-fish/1817805911?i=1817805912&theme=auto"></iframe>
+          </Flex>
+
 
           <Flex direction={'column'} alignItems={isMobile ? 'center' : 'flex-start'} justifyContent={'center'}>
             <LavaTypo variant={'h3'} size={'30px'} styles={{ marginTop: '16px' }}>
